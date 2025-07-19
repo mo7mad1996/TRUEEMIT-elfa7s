674457a74d698bc1620c29df376fb5b80fe982c5
Mohamed Ibrahim <mo7mad369@gmail.com>
2025-07-19 22:19:14 +0300

    fix install file

--- install.bat
+++ install.bat
@@ -1,1 +1,1 @@
-git pull origin main  && cls && git pull origin main && npm i && rm -rf .nuxt && npm run build && node remove.js && npm start
+powershell -Command "git pull origin main; cls; git pull origin main; npm install; if (Test-Path .nuxt) { Remove-Item -Recurse -Force .nuxt }; npm run build; node remove.js; start.bat"
