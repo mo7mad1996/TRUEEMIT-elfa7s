7b52378292239522e3c54775f6431b1a24ccf936
Mohamed Ibrahim <mo7mad369@gmail.com>
2025-07-19 21:25:23 +0300

    update install files

--- install.bat
+++ install.bat
@@ -1,1 +1,3 @@
-powershell -Command "$connection = Get-NetTCPConnection -LocalPort 3000 -ErrorAction SilentlyContinue; if ($connection) { Stop-Process -Id $connection.OwningProcess -Force }; git pull origin main; cls; git pull origin main; npm i; Remove-Item -Recurse -Force .nuxt; npm run build; node remove.js; npm start"
+powershell $connection = Get-NetTCPConnection -LocalPort 3000 -ErrorAction SilentlyContinue
+if ($connection) { Stop-Process -Id $connection.OwningProcess -Force 
+&& git pull origin main  && cls && git pull origin main && npm i && rm -rf .nuxt && npm run build && node remove.js && npm start
