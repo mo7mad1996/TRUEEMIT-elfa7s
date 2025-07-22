c673cc22bc1b907de0bd98f4da3a7e7045eaf98b
Mohamed Ibrahim <mo7mad369@gmail.com>
2025-07-22 16:19:01 +0300

    edit hr

--- plugins/nltobr.js
+++ plugins/nltobr.js
@@ -1,7 +1,7 @@
 export default (_, inject) => {
   // uncomment this
   // edit this to <br /> not <hr />
-  inject("nltobr", (text, tag = hr) =>
+  inject("nltobr", (text, tag = 'hr') =>
     text.trim().replaceAll("\n", `<${tag} />`)
   );
 };
