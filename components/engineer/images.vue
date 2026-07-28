80bd3c9d04f845384e8e2d6e57a490518bac978a
Moahemd <mo7mad369@gmail.com>
2026-07-28 16:45:57 +0300

    remove the text 🙀

--- components/engineer/images.vue
+++ components/engineer/images.vue
@@ -24,15 +24,9 @@
 						Filled, and MB indicates Replaced and Repainted.
 					</div>
 
-					<div>
-						فحص البدي يعتمد على الجهاز فقط<br />
-						فحص البدي يعتمد على قراءة نسبة الطلاء فقط.
-					</div>
+					<div>فحص البدي يعتمد على قراءة نسبة الطلاء فقط.</div>
 
-					<div class="dir-ltr">
-						The body inspection relies on the device only.<br />
-						The body inspection relies on the paint thickness reading only.
-					</div>
+					<div class="dir-ltr">The body inspection relies on the paint thickness reading only.</div>
 				</div>
 			</template>
 
@@ -41,15 +35,11 @@
 					ملاحظة : علامة M تعني مرشوش و T تعني تعديل وعلامة MM تعني مرشوش و معجون وعلامة MB تعني
 					مغير ومرشوش
 					<br />
-					فحص البدي يعتمد على الجهاز فقط
-					<br />
 					فحص البدي يعتمد على قراءة نسبة الطلاء فقط.
 				</span>
 				<span v-else class="!leading-none">
 					Note: The letter M indicates Repainted, T stands for Repaired, MM means Repainted and
 					Filled, and MB indicates Replaced and Repainted.
-					<br />
-					The body inspection relies on the device only.
 					<br />
 					The body inspection relies on the paint thickness reading only.
 				</span>
